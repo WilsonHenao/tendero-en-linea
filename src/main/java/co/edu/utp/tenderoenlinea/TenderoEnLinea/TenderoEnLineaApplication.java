6d9058c9a3d19110cf3403073385e7c01afc6968
Wilson <wilson.henao@utp.edu.co>
2020-05-08 21:29:30 -0500

    Inicializando repositorio

---
+++ src/main/java/co/edu/utp/tenderoenlinea/TenderoEnLinea/TenderoEnLineaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.tenderoenlinea.TenderoEnLinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderoEnLineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderoEnLineaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/tenderoenlinea/TenderoEnLinea/TenderoEnLineaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.tenderoenlinea.TenderoEnLinea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderoEnLineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
